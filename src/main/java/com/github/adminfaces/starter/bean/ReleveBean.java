bfb5db83a091374b384a4017751461227cd70060
calviniloki <calviniloki@localhost>
2020-03-23 09:50:02 +0100

    Signed-off-by:calviniloki <calviniloki@localhost>

--- src/main/java/com/github/adminfaces/starter/bean/ReleveBean.java
+++ src/main/java/com/github/adminfaces/starter/bean/ReleveBean.java
@@ -19,7 +19,9 @@ import com.github.adminfaces.starter.service.ExamenService;
 import com.github.adminfaces.starter.service.NoteService;
 import java.io.Serializable;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 import javax.annotation.PostConstruct;
 import javax.enterprise.context.SessionScoped;
@@ -38,6 +40,7 @@ public class ReleveBean implements Serializable{
     private Matiere matiere;
     private Integer trimestre;
     private List<Eleve> list ;
+     int i=0;
     
     @Inject
     ExamenService examenService;
@@ -55,6 +58,7 @@ public class ReleveBean implements Serializable{
     @Service
     CrudService<Examen, Integer> examenCrudService;
     
+    private Map<Integer,Double> listRang;
     @PostConstruct
     public void init(){
         list = new ArrayList<>();
@@ -62,6 +66,24 @@ public class ReleveBean implements Serializable{
     
     public void genererReleve(){
         list = eleveService.listeParClasse(classe);
+        List<Double> listMoy = new ArrayList<>();
+        list.forEach(e->{
+            double moy = (findDisciplineByClasseMatiere(classe, matiere))
+                .getCoefficient() * ((noteExamenParEleve(e, "COMPOSITION")
+                +(((noteExamenParEleve(e, "INTERRO 1")
+                + noteExamenParEleve(e, "INTERRO 2"))/2)
+                + noteExamenParEleve(e, "EVALUATION"))/2)/2);
+            listMoy.add(moy);
+        });
+        listMoy.sort((o1, o2) -> {
+            return o1.compareTo(o2); //To change body of generated lambdas, choose Tools | Templates.
+        });
+         listRang = new HashMap();
+       
+        
+        listMoy.forEach(r->{
+            listRang.put(++i, r);
+        });
     }
     
     public Discipline findDisciplineByClasseMatiere(Classe classe, Matiere matiere){
@@ -92,6 +114,7 @@ public class ReleveBean implements Serializable{
             return 0.0;
         }
     }
+    
 
     public Classe getClasse() {
         return classe;
@@ -123,6 +146,14 @@ public class ReleveBean implements Serializable{
 
     public void setList(List<Eleve> list) {
         this.list = list;
+    }
+
+    public Map<Integer,Double> getListRang() {
+        return listRang;
+    }
+
+    public void setListRang(Map<Integer,Double> listRang) {
+        this.listRang = listRang;
     }
     
     
